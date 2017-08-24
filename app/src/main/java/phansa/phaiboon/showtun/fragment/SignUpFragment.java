30c711c841283745d93e5a0b7be4dfb278776f78
MasterUNG <phrombutr@gmail.com>
2017-08-24 14:12:10 +0700

    Check Space OK

--- app/src/main/java/phansa/phaiboon/showtun/fragment/SignUpFragment.java
+++ app/src/main/java/phansa/phaiboon/showtun/fragment/SignUpFragment.java
@@ -7,15 +7,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import phansa.phaiboon.showtun.MainActivity;
 import phansa.phaiboon.showtun.R;
+import phansa.phaiboon.showtun.manager.MyAlert;
 
 /**
  * Created by masterung on 8/23/2017 AD.
  */
 
 public class SignUpFragment extends Fragment{
+
+    //Explicit
+    private String nameString, userString, passwordString, rePasswordString;
+
+
+
 
     @Nullable
     @Override
@@ -33,7 +42,44 @@ public class SignUpFragment extends Fragment{
         //Create ToolBar
         createToolBar();
 
+        //SignUp Controller
+        signUpController();
+
     }   // onActivityCreate
+
+    private void signUpController() {
+        Button button = getView().findViewById(R.id.btnSignUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Initial EditText view
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+                EditText rePasswordEditText = getView().findViewById(R.id.edtRePassword);
+
+                //Get Value From EditText
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+                rePasswordString = rePasswordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") ||
+                        userString.equals("") ||
+                        passwordString.equals("") || rePasswordString.equals("")) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getString(R.string.title_have_space),
+                            getString(R.string.message_have_space));
+                }
+
+
+
+            }   // onClick
+        });
+    }
 
     private void createToolBar() {
         //SetUp Toolbar
